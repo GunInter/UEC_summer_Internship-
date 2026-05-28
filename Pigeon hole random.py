83ca7b133529092958f29a5ce1e5681cc78c6b5b
GunInter <73353279+GunInter@users.noreply.github.com>
2026-05-29 02:25:24 +0900

    Create Pigeon hole random.py

---
+++ Pigeon hole random.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+# inputs of the matrix dimensions :)
+
+n = int(input('Pls enter value n:'))
+k = int(input('Pls enter value k:'))
+m = int(input('Pls enter value m:'))
+
+# create empty matrix
+matrix = np.zeros((n, m), dtype=int)
+
+# loop m times to fill each column
+for i in range(m):
+    positions = np.random.choice(n, k, replace=False)
+    x = np.zeros(n, dtype=int)
+    x[positions] = 1
+    matrix[ :, i] = x  # save column i
+
+print('Generated matrix:')
+print(matrix)
